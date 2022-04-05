f95e79d95b2e835dc492b9cff3ef27ccdd3be188
Filipa Lopes <filipamnmlopes@hotmail.com>
2022-04-05 12:13:50 +0100

    Create model to store tasks.

--- todolist/api/models.py
+++ todolist/api/models.py
@@ -1,3 +1,17 @@
+"""The Task model is used to store all the tasks."""
+
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    IN_PROGRESS = "IN_PROGRESS"
+    COMPLETED = "COMPLETED"
+    STATUS_CHOICES = ((IN_PROGRESS, 'In-progress'), (COMPLETED, 'Completed'))
+
+    status = models.CharField(max_length=11, choices=STATUS_CHOICES, default=IN_PROGRESS)
+    name = models.TextField()
+
+    def __str__(self):
+        return self.name
+
